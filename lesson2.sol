9f90d70b8ac78bfd9551572632bb4c164babfce0
Thompson <140930314+Godbrand0@users.noreply.github.com>
2025-07-30 11:45:11 +0100

    deployed another contract

---
+++ lesson2.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract Lesson2 {
+   string public constant HELLO_WORLD = "Hello, World!";
+// This contract defines a constant string variable HELLO_WORLD with the value "Hello, World!, make sure the variable name is in capital letters"
+
+
+}
+
--- transferOwner.sol
+++ transferOwner.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
+pragma solidity 0.8.20;
 
 
 // This Solidity code demonstrates a simple ownership transfer mechanism using a contract called Ownable.
